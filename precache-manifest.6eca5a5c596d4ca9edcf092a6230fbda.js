246146a422825ada314ea10734d8caf2ecf5eb77
Warisa <warisa.pae@mail.kmutt.ac.th>
2019-10-25 18:58:50 +0700

    [ADD] Add build

---
+++ precache-manifest.6eca5a5c596d4ca9edcf092a6230fbda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d12ad4a9b57d9abd079d83dc574b24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cf888e39f0fe4325434",
+    "url": "/static/css/main.a6e7ba6c.chunk.css"
+  },
+  {
+    "revision": "9be3d3b2fd2285739925",
+    "url": "/static/js/2.8b9cbeea.chunk.js"
+  },
+  {
+    "revision": "4cf888e39f0fe4325434",
+    "url": "/static/js/main.de56e5bb.chunk.js"
+  },
+  {
+    "revision": "a7b03acbaa2623c39c05",
+    "url": "/static/js/runtime-main.a17de5ef.js"
+  }
+]);
